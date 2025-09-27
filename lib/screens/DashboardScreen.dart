9869fa7d30de7fef9913624487243a09d70011c9
alwjhmmr29-create <alwjhmmr29@gmail.com>
2025-09-27 22:27:37 +0300

    👤 إضافة منطق استقبال اسم المستخدم من ModalRoute

--- lib/screens/DashboardScreen.dart
+++ lib/screens/DashboardScreen.dart
@@ -3,3 +3,6 @@ import 'package:flutter/material.dart';
 class DashboardScreen extends StatelessWidget {
   static const routeName = '/dashboard';
   const DashboardScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    final username = ModalRoute.of(context)!.settings.arguments as String? ?? 'المستخدم';
