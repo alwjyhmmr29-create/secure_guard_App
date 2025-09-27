c4fc3ba14f0143f465acdfa6f1988a11a35cb187
alwjhmmr29-create <alwjhmmr29@gmail.com>
2025-09-27 22:21:45 +0300

    🔑 إضافة formKey و TextEditingControllers (username & password)

--- lib/screens/LoginScreen.dart
+++ lib/screens/LoginScreen.dart
@@ -7,3 +7,14 @@ class LoginScreen extends StatefulWidget {
   @override
   State<LoginScreen> createState() => _LoginScreenState();
 }
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
