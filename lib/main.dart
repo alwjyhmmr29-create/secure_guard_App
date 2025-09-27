f72d4a176c041d227adca26e95ced668d73ecdd0
alwjhmmr29-create <alwjhmmr29@gmail.com>
2025-09-27 22:07:12 +0300

    📂 استدعاء جميع الشاشات (Login, Dashboard, PasswordCheck, LinkCheck, About)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/cupertino.dart';
-
+import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
+import 'screens/dashboard_screen.dart';
+import 'screens/password_check_screen.dart';
+import 'screens/link_check_screen.dart';
+import 'screens/about_screen.dart';
 void main() {
   runApp(const SecureGuardApp());
 }
