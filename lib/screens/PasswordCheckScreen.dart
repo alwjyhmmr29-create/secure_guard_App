0276cbde51f18411f172d3d15757b46b482db5e2
alwjhmmr29-create <alwjhmmr29@gmail.com>
2025-09-27 22:31:22 +0300

    🔑 إضافة منطق فحص قوة كلمة المرور (evaluatePasswordStrength)

--- lib/screens/PasswordCheckScreen.dart
+++ lib/screens/PasswordCheckScreen.dart
@@ -11,3 +11,29 @@ class PasswordCheckScreen extends StatefulWidget {
 class _PasswordCheckScreenState extends State<PasswordCheckScreen> {
   final _controller = TextEditingController();
   String? _result;
+  void _checkPassword() {
+    final p = _controller.text;
+    setState(() {
+      _result = evaluatePasswordStrength(p);
+    });
+  }
+
+  String evaluatePasswordStrength(String p) {
+    if (p.isEmpty) return 'فارغة';
+    int score = 0;
+    if (p.length >= 8) score += 2;
+    if (p.length >= 12) score += 1;
+    if (RegExp(r'[A-Z]').hasMatch(p)) score += 1;
+    if (RegExp(r'[0-9]').hasMatch(p)) score += 1;
+    if (RegExp(r'[!@#\$%\^&\*(),.?":{}|<>]').hasMatch(p)) score += 2;
+
+    if (score >= 6) return 'قوية';
+    if (score >= 4) return 'متوسطة';
+    return 'ضعيفة';
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
