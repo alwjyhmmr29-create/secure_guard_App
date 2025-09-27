71a71117f91468314a1a5517fefb927253cf7c80
alwjhmmr29-create <alwjhmmr29@gmail.com>
2025-09-27 22:19:04 +0300

    🛠️ إنشاء StatefulWidget باسم LoginScreen وتعريف routeName

---
+++ lib/screens/LoginScreen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  static const routeName = '/login';
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
